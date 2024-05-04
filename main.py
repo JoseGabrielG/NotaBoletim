39b50279a4430a7984bcd3b7592f76277fab0a71
José Gabriel <167626918+JoseGabrielG@users.noreply.github.com>
2024-05-04 14:59:48 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+def média (n1,n2,n3,n4):
+  return ((n1 + n2 + n3 + n4) / 4)
+
+n1 = float(input("digite sua primeira nota"))
+n2 = float(input("digite sua segunda nota"))
+n3 = float(input("digite sua terceira nota"))
+n4 = float(input("digite sua quarta nota"))
+
+print("Sua média é", média(n1,n2,n3,n4))
---
+++ peixe.py
@@ -0,0 +1,4 @@
+peso = float(input("Qual a quantidade o peso?(kg)"))
+excesso = peso - 50
+multa = excesso * 4
+print(f" Sua multa vai ser de R${multa}")
